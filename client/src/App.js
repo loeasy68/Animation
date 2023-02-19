1f41f5c4e8e1e5f161f45752362325b0f777fbf3
loeasy68 <a.jones3145@gmail.com>
2023-02-19 00:49:36 +0000

    added things

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function Editor() {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Editor />
+      </header>
+    </div>
+  );
+}
+
+export default App;
